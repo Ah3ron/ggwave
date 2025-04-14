ec8411d5f43b3ea71cd2093ef24f2f91b16d12ea
Ah3ron <ah3ron@protonmail.com>
2025-04-14 17:48:13 +0300

    Add meta info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,9 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Ggwave - High Fidelity Audio</title>
+	<meta name="description" content="Experience high fidelity audio streaming with Ggwave, your destination for cutting-edge sound technology." />
+</svelte:head>
+
 {@render children()}
